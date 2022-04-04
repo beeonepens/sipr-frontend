aa743685700477e0d00a307aa93f45a5b8210e12
Khoirul Asfian <asfian99@gmail.com>
2022-04-04 14:42:21 +0700

    chore(global): create user & admin app

--- apps/user-web/pages/index.tsx
+++ apps/user-web/pages/index.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { Button } from "ui";
 
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1>User Client</h1>
       <Button />
     </div>
   );
