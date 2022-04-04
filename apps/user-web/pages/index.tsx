ef0a9f7d1883b546edcb065797378a664bf4e3bf
Khoirul Asfian <asfian99@gmail.com>
2022-04-04 15:30:42 +0700

    feat(global): setup eslint & prettier

--- apps/user-web/pages/index.tsx
+++ apps/user-web/pages/index.tsx
@@ -1,7 +1,14 @@
-import React from "react";
-import { Button } from "ui";
+import React, { useEffect, useState } from 'react';
+import { Button } from 'ui';
 
 export default function Web() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    setData((cv) => `Hello ${cv}`);
+    console.log(data);
+  }, [data]);
+
   return (
     <div>
       <h1>User Client</h1>
--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,12 +1,32 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  // extends: ["next", "prettier"],
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:@next/next/recommended",
+    "prettier",
+  ],
   settings: {
     next: {
       rootDir: ["apps/*/", "packages/*/"],
     },
   },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "react/jsx-filename-extension": "off",
+    "react/prop-types": 0,
+    "react/destructuring-assignment": 0,
+    "react/jsx-props-no-spreading": 0,
+    "no-console": 0,
+    "no-nested-ternary": 1,
   },
 };
