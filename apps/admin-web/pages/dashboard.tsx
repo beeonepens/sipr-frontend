831ea573ed3008d1332ffa743c377ff725af6929
Khoirul Asfian <45413544+asfian99@users.noreply.github.com>
2022-04-08 14:20:42 +0700

    feat(admin): create login & dashboard for admin user

--- apps/admin-web/pages/dashboard.tsx
+++ apps/admin-web/pages/dashboard.tsx
@@ -5,19 +5,18 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
-export default function HomePage() {
+export default function Dashboard() {
   return (
     <Container sx={{ padding: '4rem' }}>
       <Stack direction="column" mb="1rem" spacing={1}>
-        <Typography variant="h4">Welcome to</Typography>
-        <Typography variant="h2" component="h1" sx={{ fontWeight: 'bold' }}>
-          SIPR Admin Page
+        <Typography variant="h3" component="h1" sx={{ fontWeight: 500 }}>
+          Admin Dashboard Page
         </Typography>
       </Stack>
 
-      <Link href="/login" passHref>
-        <Button size="large" LinkComponent="a">
-          Login
+      <Link href="/" passHref>
+        <Button size="large" color="error" LinkComponent="a">
+          Logout
         </Button>
       </Link>
     </Container>
--- apps/admin-web/pages/login.tsx
+++ apps/admin-web/pages/login.tsx
@@ -1,14 +1,56 @@
 import React from 'react';
+import Link from 'next/link';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 export default function Login() {
   return (
-    <Container sx={{ padding: '4rem' }}>
-      <Stack direction="column" spacing={2}>
-        <Typography variant="h3">Login</Typography>
-      </Stack>
+    <Container>
+      <Box
+        sx={{
+          minHeight: '100vh',
+          width: '40%',
+          mx: 'auto',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Card variant="outlined" sx={{ paddingY: '1rem', paddingX: '0.5rem' }}>
+          <CardContent sx={{ marginX: '1.5rem', textAlign: 'center' }}>
+            <Typography variant="h4" component="h1" sx={{ fontWeight: 500 }}>
+              Login
+            </Typography>
+
+            <Stack direction="column" my="2rem" spacing={2}>
+              <TextField
+                id="email"
+                label="Email"
+                variant="outlined"
+                type="text"
+              />
+              <TextField
+                id="password"
+                label="Password"
+                variant="outlined"
+                type="password"
+              />
+            </Stack>
+
+            <Link href="/dashboard" passHref>
+              <Button fullWidth LinkComponent="a">
+                Login
+              </Button>
+            </Link>
+          </CardContent>
+        </Card>
+      </Box>
     </Container>
   );
 }
