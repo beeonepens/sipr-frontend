dfbab8a23141b81f2f4d7e477b19591bdbb2bf54
Khoirul Asfian <45413544+asfian99@users.noreply.github.com>
2022-04-08 23:20:23 +0700

    refactor(admin): remove mui and setup tailwindcss in admin page

--- apps/admin-web/pages/dashboard.tsx
+++ apps/admin-web/pages/dashboard.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
 
 export default function Dashboard() {
   return (
-    <Container sx={{ padding: '4rem' }}>
-      <Stack direction="column" mb="1rem" spacing={1}>
-        <Typography variant="h3" component="h1" sx={{ fontWeight: 500 }}>
-          Admin Dashboard Page
-        </Typography>
-      </Stack>
-
+    <div>
+      <h1>Dashboard Page</h1>
       <Link href="/" passHref>
-        <Button size="large" color="error" LinkComponent="a">
-          Logout
-        </Button>
+        <a href="/">Logout</a>
       </Link>
-    </Container>
+    </div>
   );
 }
--- apps/admin-web/pages/index.tsx
+++ apps/admin-web/pages/index.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
 
 export default function HomePage() {
   return (
-    <Container sx={{ padding: '4rem' }}>
-      <Stack direction="column" mb="1rem" spacing={1}>
-        <Typography variant="h4">Welcome to</Typography>
-        <Typography variant="h2" component="h1" sx={{ fontWeight: 'bold' }}>
-          SIPR Admin Page
-        </Typography>
-      </Stack>
-
+    <div>
+      <h4>Welcome to</h4>
+      <h1>SIPR Admin Page</h1>
       <Link href="/login" passHref>
-        <Button size="large" LinkComponent="a">
-          Login
-        </Button>
+        <a href="/">Login</a>
       </Link>
-    </Container>
+    </div>
   );
 }
