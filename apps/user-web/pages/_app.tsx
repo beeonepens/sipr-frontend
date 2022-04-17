2338b039bae499c0f5c8dc90d68074002ab819e2
Khoirul Asfian <45413544+rulasfia@users.noreply.github.com>
2022-04-17 16:54:57 +0700

    feat(user): install & setup react-query-v4 (beta)

--- apps/user-web/pages/_app.tsx
+++ apps/user-web/pages/_app.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
 import clsx from 'clsx';
 import { useRouter } from 'next/router';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { isPublicUrl } from '@utils/constant';
 import Sidebar from '@components/organisms/Sidebar';
 import Header from '@components/organisms/Header';
@@ -9,25 +9,29 @@ import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
+const queryClient = new QueryClient();
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
 
   return (
-    <div
-      className={clsx(
-        'min-h-screen font-sans text-gray-900',
-        !isPublicUrl(pathname)
-          ? 'flex min-h-screen flex-row justify-between'
-          : ''
-      )}
-    >
-      {/* only show sidebar in non-public url */}
-      {!isPublicUrl(pathname) && <Sidebar />}
-      <main className="relative z-0 min-h-screen w-full duration-300">
-        {/* only show header in non-public url */}
-        {!isPublicUrl(pathname) && <Header />}
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div
+        className={clsx(
+          'min-h-screen font-sans text-gray-900',
+          !isPublicUrl(pathname)
+            ? 'flex min-h-screen flex-row justify-between'
+            : ''
+        )}
+      >
+        {/* only show sidebar in non-public url */}
+        {!isPublicUrl(pathname) && <Sidebar />}
+        <main className="relative z-0 min-h-screen w-full duration-300">
+          {/* only show header in non-public url */}
+          {!isPublicUrl(pathname) && <Header />}
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 }
