621f6d2384a8e7c24909a625dfab6882dcad436e
Khoirul Asfian <45413544+rulasfia@users.noreply.github.com>
2022-05-02 21:16:08 +0700

    refactor(user): change agenda table component to react-table

--- apps/user-web/components/organisms/Agenda/AgendaTable.tsx
+++ apps/user-web/components/organisms/Agenda/AgendaTable.tsx
@@ -1,75 +1,138 @@
 import clsx from 'clsx';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
+import { Column, useTable } from 'react-table';
 import AgendaTableAction from '@components/molecules/Agenda/AgendaTableAction';
 import ZeroAgenda from '@components/molecules/Agenda/ZeroAgenda';
 import { formatDateWithDay, formatMeetTime } from '@utils/formatDateTime';
 import { MeetWithDate } from '@utils/types/meet.dto';
 
 interface Props {
-  events: MeetWithDate[];
+  meets: MeetWithDate[];
   handleSelectEvent: (e: MeetWithDate) => void;
   handleDeleteEvent: (e: MeetWithDate) => void;
 }
 
-function AgendaTable({ events, handleSelectEvent, handleDeleteEvent }: Props) {
-  if (events.length < 1) {
+interface DataInterface {
+  name: string;
+  date: string;
+  time: string;
+  status: string;
+  actions: JSX.Element;
+}
+
+function AgendaTable({ meets, handleSelectEvent, handleDeleteEvent }: Props) {
+  const columns: Array<Column<DataInterface>> = useMemo(
+    () => [
+      {
+        Header: 'Meeting name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Date',
+        accessor: 'date',
+      },
+      {
+        Header: 'Time',
+        accessor: 'time',
+      },
+      {
+        Header: 'Status',
+        accessor: 'status',
+      },
+      {
+        Header: 'Actions',
+        accessor: 'actions',
+      },
+    ],
+    []
+  );
+
+  const data = useMemo(
+    () =>
+      meets.map((meet) => ({
+        name: meet.name_meeting,
+        date: formatDateWithDay(meet.start_datetime),
+        time: formatMeetTime(meet.start_datetime, meet.end_datetime),
+        status: meet.isOnline === 1 ? 'Online' : 'Offline',
+        actions: (
+          <AgendaTableAction
+            event={meet}
+            handleDeleteEvent={handleDeleteEvent}
+            handleSelectEvent={handleSelectEvent}
+          />
+        ),
+      })),
+    [meets, handleDeleteEvent, handleSelectEvent]
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
+  if (meets.length < 1) {
     return <ZeroAgenda />;
   }
   return (
     <div className="relative mt-4 overflow-x-auto rounded-md border border-gray-300 shadow-md shadow-gray-300/25 dark:border-gray-700 dark:shadow-black/20 sm:rounded-lg">
-      <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+      <table
+        {...getTableProps()}
+        className="w-full text-left text-sm text-gray-500 dark:text-gray-400"
+      >
         <thead className="bg-gray-100 text-xs uppercase text-gray-700 dark:bg-gray-900 dark:text-gray-300">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Meeting name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Date
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Time
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Status
-            </th>
-            <th scope="col" className="px-6 py-3 text-right">
-              Action
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {events.map((event) => (
-            <tr
-              key={event.id_meet}
-              className={clsx(
-                'bg-white dark:bg-gray-800',
-                events[events.length - 1].id_meet !== event.id_meet &&
-                  'border-b dark:border-gray-700'
-              )}
-            >
-              <th
-                scope="row"
-                className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
-              >
-                {event.name_meeting}
-              </th>
-              <td className="px-6 py-4">
-                {formatDateWithDay(event.start_datetime)}
-              </td>
-              <td className="px-6 py-4">
-                {formatMeetTime(event.start_datetime, event.end_datetime)}
-              </td>
-              <td className="px-6 py-4">
-                {event.isOnline ? 'Online' : 'Offline'}
-              </td>
-
-              <AgendaTableAction
-                event={event}
-                handleDeleteEvent={handleDeleteEvent}
-                handleSelectEvent={handleSelectEvent}
-              />
+          {/* Loop over the header rows */}
+          {headerGroups.map((headerGroup) => (
+            // Apply the header row props
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {/* Loop over the headers in each row */}
+              {headerGroup.headers.map((column) => (
+                // Apply the header cell props
+                <th
+                  {...column.getHeaderProps()}
+                  className={clsx(
+                    'px-6 py-3',
+                    column.render('Header') === 'Actions' && 'text-right'
+                  )}
+                >
+                  {/* Render the header */}
+                  {column.render('Header')}
+                </th>
+              ))}
             </tr>
           ))}
+        </thead>
+        {/* Apply the table body props */}
+        <tbody {...getTableBodyProps()}>
+          {/* Loop over the table rows */}
+          {rows.map((row) => {
+            // Prepare the row for display
+            prepareRow(row);
+            return (
+              // Apply the row props
+              <tr
+                {...row.getRowProps()}
+                className={clsx(
+                  'bg-white dark:bg-gray-800',
+                  meets.length - 1 !== row.index &&
+                    'border-b dark:border-gray-700'
+                )}
+              >
+                {/* Loop over the rows cells */}
+                {row.cells.map((cell) => (
+                  // Apply the cell props
+                  <td
+                    {...cell.getCellProps()}
+                    className={clsx(
+                      'px-4 py-3',
+                      cell.column.id === 'name' &&
+                        'font-medium tracking-wide text-gray-900 dark:text-gray-100'
+                    )}
+                  >
+                    {/* Render the cell contents */}
+                    {cell.render('Cell')}
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
