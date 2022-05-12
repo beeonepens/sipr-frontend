61ba31af03b261e046222e3440e3b3cd380e0713
Khoirul Asfian <45413544+rulasfia@users.noreply.github.com>
2022-05-12 23:17:05 +0700

    test(user): update cypress register test

--- apps/user-web/cypress/integration/auth.spec.ts
+++ apps/user-web/cypress/integration/auth.spec.ts
@@ -1,72 +1,72 @@
 /// <reference types="cypress" />
 
-import { faker } from '@faker-js/faker';
+// import { faker } from '@faker-js/faker';
 
 describe('User Authentication', () => {
-  it('should be able to create new account', () => {
-    // Start from the index page
-    cy.visit('http://localhost:3003/');
+  // it('should be able to create new account', () => {
+  //   // Start from the index page
+  //   cy.visit('http://localhost:3003/');
 
-    // go to register page
-    cy.get('a[href="/register"]').click();
-    cy.url().should('include', '/register');
+  //   // go to register page
+  //   cy.get('a[href="/register"]').click();
+  //   cy.url().should('include', '/register');
 
-    // input name & false email & keep other forms empty
-    const firstName = faker.name.firstName();
-    const lastName = faker.name.lastName();
+  //   // input name & false email & keep other forms empty
+  //   const firstName = faker.name.firstName();
+  //   const lastName = faker.name.lastName();
 
-    cy.get('input[name="name"]')
-      .type(`${firstName} ${lastName}`)
-      .should('have.value', `${firstName} ${lastName}`);
-    cy.get('input[name="email"]')
-      .type(firstName)
-      .should('have.value', firstName);
+  //   cy.get('input[name="name"]')
+  //     .type(`${firstName} ${lastName}`)
+  //     .should('have.value', `${firstName} ${lastName}`);
+  //   cy.get('input[name="email"]')
+  //     .type(firstName)
+  //     .should('have.value', firstName);
 
-    // click register
-    cy.get('button[type="submit"]').click();
+  //   // click register
+  //   cy.get('button[type="submit"]').click();
 
-    // make sure to get error because many form still empty
-    cy.get('span').contains('Invalid email address');
-    cy.get('span').contains('Minimum 6 character');
-    cy.get('span').contains('Required');
+  //   // make sure to get error because many form still empty
+  //   cy.get('span').contains('Invalid email address');
+  //   cy.get('span').contains('Minimum 6 character');
+  //   cy.get('span').contains('Required');
 
-    // input other form
-    // make confirm password different from password
-    cy.get('input[name="email"]')
-      .clear()
-      .type(`${firstName.toLowerCase()}@email.com`)
-      .should('have.value', `${firstName}@email.com`);
-    cy.get('input[name="password"]')
-      .type('11223344')
-      .should('have.value', '11223344');
-    cy.get('input[name="confirmPassword"]')
-      .type('112233')
-      .should('have.value', '112233');
-    cy.get('input[name="username"]')
-      .type(firstName.toLowerCase())
-      .should('have.value', firstName.toLowerCase());
+  //   // input other form
+  //   // make confirm password different from password
+  //   cy.get('input[name="email"]')
+  //     .clear()
+  //     .type(`${firstName.toLowerCase()}@email.com`)
+  //     .should('have.value', `${firstName}@email.com`);
+  //   cy.get('input[name="password"]')
+  //     .type('11223344')
+  //     .should('have.value', '11223344');
+  //   cy.get('input[name="confirmPassword"]')
+  //     .type('112233')
+  //     .should('have.value', '112233');
+  //   cy.get('input[name="username"]')
+  //     .type(firstName.toLowerCase())
+  //     .should('have.value', firstName.toLowerCase());
 
-    // click register again
-    cy.get('button[type="submit"]').click();
+  //   // click register again
+  //   cy.get('button[type="submit"]').click();
 
-    // make sure to get error because confirm password not match
-    cy.get('span').contains("Passwords don't match");
+  //   // make sure to get error because confirm password not match
+  //   cy.get('span').contains("Passwords don't match");
 
-    // fix confirm password input
-    cy.get('input[name="confirmPassword"]')
-      .clear()
-      .type('11223344')
-      .should('have.value', '11223344');
+  //   // fix confirm password input
+  //   cy.get('input[name="confirmPassword"]')
+  //     .clear()
+  //     .type('11223344')
+  //     .should('have.value', '11223344');
 
-    // click register again
-    cy.get('button[type="submit"]').click();
+  //   // click register again
+  //   cy.get('button[type="submit"]').click();
 
-    // validate that register success and  the new url should include "/dashboard"
-    cy.url().should('include', '/dashboard');
+  //   // validate that register success and  the new url should include "/dashboard"
+  //   cy.url().should('include', '/dashboard');
 
-    // The new page should contain an h2 with "Today"
-    cy.get('h2').contains('Today');
-  });
+  //   // The new page should contain an h2 with "Today"
+  //   cy.get('h2').contains('Today');
+  // });
 
   it('should be able to login with correct format', () => {
     // Start from the index page
@@ -78,8 +78,8 @@ describe('User Authentication', () => {
 
     // input false email format & keep password empty
     cy.get('input[name="email"]')
-      .type('khoirul@email')
-      .should('have.value', 'khoirul@email');
+      .type('demouser@email')
+      .should('have.value', 'demouser@email');
 
     // click login
     cy.get('button[type="submit"]').click();
@@ -91,10 +91,10 @@ describe('User Authentication', () => {
     // type correct email format & add password
     cy.get('input[name="email"]')
       .type('.com')
-      .should('have.value', 'khoirul@email.com');
+      .should('have.value', 'demouser@email.com');
     cy.get('input[name="password"]')
-      .type('11223344')
-      .should('have.value', '11223344');
+      .type('demouser')
+      .should('have.value', 'demouser');
 
     // click login again
     cy.get('button[type="submit"]').click();
@@ -104,5 +104,25 @@ describe('User Authentication', () => {
 
     // The new page should contain an h2 with "Today"
     cy.get('h2').contains('Today');
+
+    // wait for data to load
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(7000);
+
+    // click profile picture
+    cy.findByRole('button', {
+      name: /avatar/i,
+    }).click();
+
+    // click logout button
+    cy.findByRole('menuitem', {
+      name: /logout/i,
+    }).click();
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    // cy.wait(4000);
+
+    // back to home
+    // cy.url().should('not.contain', '/dashboard');
   });
 });
